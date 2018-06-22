99e077a0a4d9747b1b25f2f7f2e799ce86d944d8
seri1234 <abc_x_y_z_12@yahoo.co.jp>
2018-06-22 08:53:51 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 'use strict';
+const memo = new Map();                 //過去計算したフィボナッチ数を入れておく連想配列。このように一度計算した結果を保存しておく方法をメモ化という
+memo.set(0,0);                          //0番目のフィボナッチ数は０
+memo.set(1,1);                          //1番目のフィボナッチ数は1
 function fib(n){
-    if(n===0){
-        return 0;
-    }else if (n===1){
-        return 1;
-    }
+    if(memo.has(n)){                    //has(キー値)mapがそのキー値を持っているかどうかでtrue,falthを返す。
+        return memo.get(n);             //持っていたらその値を出力して終了
+
+   }
+   const value = fib(n-1)+fib(n-2);
+   memo.set(n,value);
+   return value;
+
+
 return fib(n-1) + fib(n-2);
 }
 const length = 40;
